6b8e6b3ee3245b6529463ea71d4395c130549a70
ayse sena <a.senafeyiz@gmail.com>
2019-01-25 08:23:58 +0300

    xox

---
+++ build/precache-manifest.0c553fdcd3129401ebc02896e864ff78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "772bdf491b3d9b8ce42b",
+    "url": "/xox/static/js/runtime~main.772bdf49.js"
+  },
+  {
+    "revision": "7579378c84d0d77dc91f",
+    "url": "/xox/static/js/main.7579378c.chunk.js"
+  },
+  {
+    "revision": "abd9e857442f04b96f9b",
+    "url": "/xox/static/js/1.abd9e857.chunk.js"
+  },
+  {
+    "revision": "7579378c84d0d77dc91f",
+    "url": "/xox/static/css/main.709d388a.chunk.css"
+  },
+  {
+    "revision": "109ca54c97f75260544c75d148cb9933",
+    "url": "/xox/index.html"
+  }
+];
